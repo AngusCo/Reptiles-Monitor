3052d53f64e3c7ccba3635d7f72db5a17066b746
AngusCo <anan20.tw@gmail.com>
2019-12-19 18:20:18 +0800

    get json data

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+var div_temp = document.querySelector(".temperature");
+var div_rh = document.querySelector(".humidity");
+
+setInterval(() => {
+    var requestURL = 'https://api.thingspeak.com/channels/929404/feeds.json?results=1';
+    var request = new XMLHttpRequest();
+
+    request.open('GET', requestURL, true);
+
+    request.onload = function() {
+        var data = JSON.parse(this.response);
+
+        div_temp.innerHTML = data["feeds"][0]["field1"];
+        div_rh.innerHTML = data["feeds"][0]["field2"];
+    };
+
+   request.send();
+}, 60000);
