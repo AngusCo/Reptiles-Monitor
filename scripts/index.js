1a7ba5c1519d72feea32365b3089e224b69ef4e5
AngusCo <anan20.tw@gmail.com>
2019-12-25 17:04:47 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
-var div_temp = document.querySelector(".temperature");
-var div_rh = document.querySelector(".humidity");
+const sampleDays = 5;
+const strokeWidth = 4;
+const circleRadius = 10;
+const circleOpacity = .3;
+const strokeColor = "white";
 
+var tempList = [];
+var rhList = [];
 
 function GetRealtimeValue () {
     var requestURL = 'https://api.thingspeak.com/channels/929404/feeds.json?results=1';
@@ -10,22 +15,22 @@ function GetRealtimeValue () {
     
     request.onload = function() {
         var data = JSON.parse(this.response);
-        var temp = data["feeds"][0]["field1"];
-        var rh= data["feeds"][0]["field2"];
+        var realtimeTemp = data["feeds"][0]["field1"];
+        var realtimeRh = data["feeds"][0]["field2"];
 
-        $({countNum: 0}).animate({countNum: temp}, {
+        $({countNum: 0}).animate({countNum: realtimeTemp}, {
             duration: 1000,
             easing: 'swing',
             step: function() {
-                div_temp.innerHTML = Math.round(this.countNum);
+                document.querySelector(".temperature").innerHTML = Math.round(this.countNum);
             }
         });
 
-        $({countNum: 0}).animate({countNum: rh}, {
+        $({countNum: 0}).animate({countNum: realtimeRh}, {
             duration: 1000,
             easing: 'swing',
             step: function() {
-                div_rh.innerHTML = Math.round(this.countNum);
+                document.querySelector(".humidity").innerHTML = Math.round(this.countNum);
             }
         });
     };
@@ -39,21 +44,20 @@ function GetValueList () {
     var now = new Date();
     var yymm = now.getFullYear() + "-" + (now.getMonth() + 1) + "-";
     var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    var start = "start=" + yymm + (now.getDate() - 4) + "T" + time;
+    var start = "start=" + yymm + (now.getDate() - sampleDays) + "T" + time;
     var end = "end=" + yymm + now.getDate() + "T" + time;
 
     requestURL += "&" + start + "&" + end;
     console.log(requestURL);
     
     var request = new XMLHttpRequest();
-
     request.open('GET', requestURL, true);
     
     request.onload = function() {
         var data = JSON.parse(this.response);
         var length = data.feeds.length; // 資料長度
-        var tempList = [];              // 溫度陣列
-        var rhList = [];                // 濕度陣列
+        var tempPos = [];
+        var rhPos = [];
 
         // 填入溫濕度資料
         for (var i = 0; i < length; i++) {
@@ -61,13 +65,13 @@ function GetValueList () {
             rhList.push(Math.round(data["feeds"][i]["field2"]));
         }
 
-        console.log("TEMP Before: " + tempList);
-        console.log("RH Before: " + rhList);
+        console.log("原始溫度資料: " + tempList);
+        console.log("原始濕度資料: " + rhList);
 
-        // 等比例放大 Y 軸
+        // 換算成 Y 軸座標
         for (var i = 0; i < length; i++) {
-            tempList[i] = 300 - (tempList[i] - 10) * 10;
-            rhList[i] = 300 - (rhList[i] - 40) * 5;
+            tempPos[i] = 300 - (tempList[i] - 10) * 10;
+            rhPos[i] = 300 - (rhList[i] - 40) * 5;
         }
 
         // 加入 X 軸座標
@@ -75,68 +79,80 @@ function GetValueList () {
         var spacing = screenWidth / (length + 1);
 
         for (var i = 0; i < length; i++) {
-            tempList.splice(i * 2, 0, (i + 1) * spacing);
-            rhList.splice(i * 2, 0, (i + 1) * spacing);
+            tempPos.splice(i * 2, 0, (i + 1) * spacing);
+            rhPos.splice(i * 2, 0, (i + 1) * spacing);
         }
-        console.log("TEMP After: " + tempList);
-        console.log("RH After: " + rhList);
+        console.log("溫度曲線座標[x1,y1,x2,y2...]: " + tempPos);
+        console.log("濕度曲線座標[x1,y1,x2,y2...]: " + rhPos);
 
-        DrawCurve(tempList, rhList);
+        DrawCurve(tempPos, "div-temp-curve");
+        DrawCurve(rhPos, "div-rh-curve");
     };
 
     request.send();
 }
 
-function DrawCurve (temp, rh) {
-    var tempStage = new Konva.Stage({
-        container: 'div-temp-curve',
+function DrawCurve (list, id) {
+    // Define stage
+    var stage = new Konva.Stage({
+        container: id,
         width: window.innerWidth,
         height: 300,
         // draggable: true
     });
 
-    var rhStage = new Konva.Stage({
-        container: 'div-rh-curve',
-        width: window.innerWidth,
-        height: 300,
-        // draggable: true
+    // Define layer
+    var layer = new Konva.Layer();
+
+    // Draw Curve
+    var line = new Konva.Line({
+        points: list,
+        stroke: strokeColor,
+        strokeWidth: strokeWidth,
+        lineCap: "round",
+        lineJoin: "round"
     });
+    layer.add(line);
 
-    var tempLayer = new Konva.Layer();
-    var rhLayer = new Konva.Layer();
-    tempStage.add(tempLayer);
-    rhStage.add(rhLayer);
+    // Draw Points
+    for (var i = 0; i < list.length; i+=2)
+    {
+        var alphaCircle = new Konva.Circle({
+            x: list[i],
+            y: list[i+1],
+            radius: circleRadius,
+            fill: strokeColor,
+            opacity: circleOpacity
+        });
+        layer.add(alphaCircle);
 
-    var tempLine = new Konva.Line({
-        points: temp,
-        stroke: 'white',
-        strokeWidth: 5,
-        lineCap: 'round',
-        lineJoin: 'round'
-    });
+        var centerCircle = new Konva.Circle({
+            x: list[i],
+            y: list[i+1],
+            radius: (circleRadius / 2),
+            fill: strokeColor,
+        });
+        layer.add(centerCircle);
 
-    var rhLine = new Konva.Line({
-        points: rh,
-        stroke: 'white',
-        strokeWidth: 5,
-        lineCap: 'round',
-        lineJoin: 'round'
-    });
+        var valueText = new Konva.Text({
+            x: list[i],
+            y: list[i+1],
+            text: tempList[i/2],
+            fontSize: 14,
+            fontFamily: "Verdana",
+            fill: strokeColor,
+        });
+        valueText.offsetX(10);
+        valueText.offsetY(-15);
+        layer.add(valueText);
+    }
 
-    tempLayer.add(tempLine);
-    rhLayer.add(rhLine);
-    tempLayer.draw();
-    rhLayer.draw();
+    // Show
+    stage.add(layer);
+    layer.draw();
 
     // 限制拖移軸向
-    // tempStage.dragBoundFunc(function(pos){
-    //     return {
-    //         x: pos.x,
-    //         y: this.absolutePosition().y
-    //     };
-    // });
-
-    // rhStage.dragBoundFunc(function(pos){
+    // stage.dragBoundFunc(function(pos){
     //     return {
     //         x: pos.x,
     //         y: this.absolutePosition().y
@@ -148,6 +164,7 @@ $(document).ready(function() {
     GetRealtimeValue();
     GetValueList();
 
+    // 每分鐘更新一次
     setInterval(() => {
         GetRealtimeValue();
     }, 60000);
